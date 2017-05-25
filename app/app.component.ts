28fd7bea58c272de293797b256c7b429bc75d833
Victor Guzman <viaguz@gmail.com>
2017-05-25 11:55:15 -0600

    Add products service to retrieve data.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
+import { ProductService } from './products/product.service';
+
 @Component({
     selector: 'pm-app',
     template: `
     <div><h1>{{pageTitle}}</h1>
         <pm-products></pm-products>
-    </div>
-    `
+    </div>`,
+    providers: [ ProductService ]
 })
 
 export class AppComponent {
