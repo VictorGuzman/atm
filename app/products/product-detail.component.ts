e2064ddc855e6b8587afa3a8a91384a2b51189b8
Victor Guzman <viaguz@gmail.com>
2017-05-29 08:38:59 -0600

    Add routing with params to product detail page.

--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { IProduct } from './product';
 
 @Component({
     templateUrl: 'app/products/product-detail.component.html'
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
     pageTitle: string = 'Product Detail';
     product: IProduct;
+
+    constructor(private _route: ActivatedRoute) {
+    }
+
+    ngOnInit(): void {
+        let id = +this._route.snapshot.params['id'];
+        this.pageTitle += `: ${id}`;
+    }
 }
